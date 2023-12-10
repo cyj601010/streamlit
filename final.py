3637eb748b282d7d810148ae9992cb1ccc90dad6
cyj601010 <98785062+cyj601010@users.noreply.github.com>
2023-12-10 18:40:05 -0500

    Update final.py

--- final.py
+++ final.py
@@ -5,14 +5,15 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# %% [markdown]
- 
-# %%
+
 st.markdown("# Welcome to my streamlit app!")
 
 st.markdown("# 1. Read in the data, call the dataframe "s"  and check the dimensions of the dataframe ")
-s=pd.read_csv("https://raw.githubusercontent.com/cyj601010/streamlit/main/social_media_usage.csv")
-print(s.head())
+
+#s=pd.read_csv("https://raw.githubusercontent.com/cyj601010/streamlit/main/social_media_usage.csv")
+
+
+#print(s.head())
 
 
 # %%
